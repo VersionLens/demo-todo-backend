c1c37510a99318496f39e25669fa337b78b48af0
Fredrik Stockman <fredrikstockman@gmail.com>
2022-12-05 18:13:44 +0100

    GraphQL mutation to create todo

--- django/main/schema.py
+++ django/main/schema.py
@@ -1,9 +1,17 @@
 import strawberry
 from typing import List
 from .types import Todo
+from main.models import Todo as TodoModel
 
 @strawberry.type
 class Query:
     todos: List[Todo] = strawberry.django.field()
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def add_todo(self, title: str) -> Todo:
+        todo = TodoModel.objects.create(title=title)
+        return todo
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
